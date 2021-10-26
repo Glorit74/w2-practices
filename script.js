c37e5c83fb6673953f3c391488e78ba36c820bf8
laszlo.t.flora@gmail.com <laszlo.t.flora@gmail.com>
2021-10-26 15:04:29 +0200

    new toggle functions

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+function fastLoad(text) {
+  console.log(text);
+  return "Text logged";
+}
 function loadEvent() {
     console.log('the page has loaded');
+   
     function onClick() {
       document.getElementById("menu-btn").classList.toggle("clicked");
+    const fastLogResult = "Ezt akarjuk kiírni";
+    console.log(fastLogResult);
     };
-    document.getElementById("menu-btn").addEventListener("click", onClick);
-
-// document.getElementById("menu-btn").addEventListener("click", function(){
-//   document.getElementById("menu-btn").classList.toggle("onclicked");
-// });
-
-}; 
+    // function onClickToggle(event) {
+      //   event.getElementById("menu-btn").classList.toggle("clicked");
+      // };
+      document.getElementById("menu-btn").addEventListener("click", onClick);
+      
+      // document.getElementById("menu-btn").addEventListener("click", function(){
+        //   document.getElementById("menu-btn").classList.toggle("onclicked");
+        // });
+        
+      }; 
+fastLoad("Na ez minden függvényen kívül van");      
 window.addEventListener("load", loadEvent);
