e6f684fa739655c36c3f65cbd75c3fea2354c642
laszlo.t.flora@gmail.com <laszlo.t.flora@gmail.com>
2021-10-27 21:29:50 +0200

    toggle with css selector

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 function onClickToggle() {
     const body = document.getElementById("bodyId");
     body.classList.toggle("clicked");
-
-    if (body.getAttribute("class") == "clicked") {
-        document.querySelector("#menu-btn").style.backgroundColor = "red";
-        document.querySelector("#nav").style.setProperty("display", "none");
-      }
-      else {
-        document.querySelector("#menu-btn").style.backgroundColor = "black"; 
-        document.querySelector("#nav").style.setProperty("display", "inline");
-     }
   }
   
   function loadEvent() {
