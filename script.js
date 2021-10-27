46bd6bb09b72a309da4cfd5a320d373b6a5522cb
laszlo.t.flora@gmail.com <laszlo.t.flora@gmail.com>
2021-10-27 16:42:32 +0200

    valami

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function onClickToggle() {
     body.classList.toggle("clicked");
 
     if (body.getAttribute("class") == "clicked") {
-        document.querySelector("#menu-btn").style.backgroundColor = "red";
-        document.querySelector("#nav").style.setProperty("display", "none");
-      }
-      else {
-        document.querySelector("#menu-btn").style.backgroundColor = "black"; 
-        document.querySelector("#nav").style.setProperty("display", "inline");
-     }
-  }
+      document.querySelector("#menu-btn").style.backgroundColor = "red";
+      document.querySelector("#nav").style.setProperty("display", "inline");
+    }
+    else {
+      document.querySelector("#menu-btn").style.backgroundColor = "black"; 
+      document.querySelector("#nav").style.setProperty("display", "none");
+   }
+}
   
   function loadEvent() {
     // console.log('the page has loaded');
